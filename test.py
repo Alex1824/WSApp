c357de94a214848d99ba9aecb1f6b45ba8c618c3
Shinobi1824 <ing.carloshern@gmail.com>
2025-05-01 23:52:40 -0300

    cumplio su fin

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.managers.language_manager import LanguageManager
-
-
-class textos(LanguageManager):
-    
-    def imprmir():
-        mg = LanguageManager()
-        
-        print(dir(mg))
